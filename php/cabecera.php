8563ff8e8ed60023872c1dc312807c5bf261145b
Vladimir Hidalgo <vlad@todosv.com>
2010-09-29 21:30:22 -0600

    Cabecera/Reloj/Timezones - mas zonas

--- php/cabecera.php
+++ php/cabecera.php
@@ -6,14 +6,16 @@
 		$(function($) {
 		    $('#jclock').jclock({timeNotation: '12h',am_pm: true,utc: true,utc_offset: -6});
 		    $('#jclock2').jclock({timeNotation: '12h',am_pm: true,utc: true,utc_offset: -7});
-		    $('#jclock3').jclock({timeNotation: '12h',am_pm: true,utc: true,utc_offset: -4});
+		    $('#jclock3').jclock({timeNotation: '12h',am_pm: true,utc: true,utc_offset: -5});
+		    $('#jclock4').jclock({timeNotation: '12h',am_pm: true,utc: true,utc_offset: -4});
 		});
 		</script>
 
 		<table class="mini tabla-estandar">
-		    <tr><th>San Salvador</th><td><span id="jclock"></span></td></tr>
 		    <tr><th>California</th><td><span id="jclock2"></span></td></tr>
-		    <tr><th>New York</th><td><span id="jclock3"></span></td></tr>
+		    <tr><th>San Salvador</th><td><span id="jclock"></span></td></tr>
+		    <tr><th>Chicago</th><td><span id="jclock3"></span></td></tr>
+		    <tr><th>Virginia</th><td><span id="jclock4"></span></td></tr>
 		</table>
             </td>
 	    <td id="centro">
